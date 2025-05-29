9b9247da6602f2f4a4b93f9bde0ca0f783b5ccef
Ruslan Skytsko <Gaveloshpom@gmail.com>
2025-05-29 18:14:06 +0200

    Add project files.

---
+++ DomainTables/Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace DomainTables
+{
+    public class Car
+    {
+        [Key]
+        public string CarNumber { get; set; } 
+
+        [Required]
+        public string Brand { get; set; }
+
+        [Required]
+        public string CarType { get; set; } // "легковий", "вантажний", "автобус"
+
+        [Required]
+        public DateTime ReleaseYearMonth { get; set; } // YYYY-MM-01
+
+        [Range(10, 1000)]
+        public int EnginePower { get; set; }
+
+        [Range(1.0, 99.9)]
+        public decimal FuelConsumption { get; set; } // літри на 100 км
+
+        public ICollection<Departure> Departures { get; set; }
+    }
+
+}
